36dd97163318f4756e22c0ca03a685f73cff1149
Christopher Hoover <ch@murgatroid.com>
2025-11-29 20:33:45 -0800

    wip

--- src/collector/writer.rs
+++ src/collector/writer.rs
@@ -38,8 +38,10 @@ impl VmWriter {
 
             let samples = self.buffer.drain_all();
             if samples.is_empty() {
+                tracing::trace!("Buffer empty, waiting...");
                 continue;
             }
+            tracing::debug!("Draining {} samples from buffer", samples.len());
 
             match self.write_samples(&samples).await {
                 Ok(()) => {
@@ -70,6 +72,7 @@ impl VmWriter {
 
     async fn write_samples(&self, samples: &[BatteryInfo]) -> Result<(), String> {
         let body = batch_to_influx(samples);
+        tracing::debug!("POST {} ({} bytes)", self.url, body.len());
 
         let response = self
             .client
